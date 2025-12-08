18a29ec33a809f309d3e3a3bba0f050911da853f
Andrew Fletcher <andrew.fletcher@kps.com>
2025-12-08 13:28:16 +0000

    Added day 8 part 1

---
+++ day8/README.md
@@ -0,0 +1,6 @@
+Run with
+```
+kotlinc -J-Xmx512M -script day8.kts input.txt
+```
+
+Increased heap size is needed to load all of the distances.
---
+++ day8/day8.kts
@@ -0,0 +1,77 @@
+import java.io.File
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+val fileName = args[0]
+val points = File(fileName).useLines { lines ->
+    lines.map { line ->
+        val (x, y, z) = line.split(",").map { it.toInt() }
+        Point(x, y, z)
+    }.toMutableList()
+}
+val distances = mutableMapOf<Circuit, Double>()
+while (points.isNotEmpty()) {
+    val pointA = points.first()
+    for (pointB in points.drop(1)) {
+        distances[Circuit(pointA, pointB)] = pointA.distanceTo(pointB)
+    }
+    points.removeAt(0)
+}
+println("Total distances: ${distances.size}")
+
+val circuits = mutableListOf<Circuit>()
+var connections = 0
+val sortedDistances = distances.entries.sortedBy { it.value }
+sortedDistances.take(1000).forEach { shortestDistance ->
+    val matches = circuits.filter { circuit ->
+        circuit.joins(shortestDistance.key)
+    }
+    if (matches.isNotEmpty()) {
+        when (matches.size) {
+            1 -> {
+                matches[0].add(shortestDistance.key)
+            }
+            2 -> {
+                val merged = matches[0]
+                merged.add(matches[1])
+                circuits.remove(matches[1])
+            }
+            else -> {
+                throw IllegalStateException("More than two matches found!")
+            }
+        }
+        connections++
+    } else {
+        circuits.add(shortestDistance.key)
+        connections++
+    }
+}
+println("Part 1: ${circuits.sortedByDescending{it.size}.take(3).fold(1) { acc, e -> acc * e.size }}")
+
+data class Point(val x: Int, val y: Int, val z: Int) {
+    fun distanceTo(other: Point): Double {
+        // I found the linked Wikipedia page to be far too confusing
+        // https://www.calculatorsoup.com/calculators/geometry-solids/distance-two-points.php
+        return sqrt(
+            (this.x - other.x).toDouble().pow(2.0) +
+                (this.y - other.y).toDouble().pow(2.0) +
+                (this.z - other.z).toDouble().pow(2.0)
+        )
+    }
+}
+data class Circuit(var points: MutableSet<Point>) {
+    constructor(first: Point, second: Point) : this(mutableSetOf(first, second))
+
+    fun add(other: Circuit) {
+        points.addAll(other.points)
+    }
+    fun joins(other: Circuit): Boolean {
+        return other.points.any { points.contains(it) }
+    }
+    val size: Int
+        get() = this.points.size
+    override fun toString(): String {
+        return "Circuit(size=$size)"
+    }
+
+}
